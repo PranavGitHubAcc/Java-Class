6b8fc9c0b3c6acdec3556e071fa7d9d09dd19e14
Pranav Mahajan <pranavmahajan18@gmail.com>
2024-02-23 10:16:18 +0530

    Update LoginDemo.java

--- LoginDemo.java
+++ LoginDemo.java
@@ -4,7 +4,6 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.Scanner;
-
 import com.mysql.cj.jdbc.AbandonedConnectionCleanupThread;
 
 
@@ -19,26 +18,55 @@ public class LoginDemo {
 			Connection connection = DriverManager.getConnection(url, "root", Password.getPass());
 			System.out.println("Connection Established");
 			Statement statement = connection.createStatement();
-			ResultSet resultSet = statement.executeQuery("select * from login");
+//			ResultSet resultSet = statement.executeQuery("select * from login");
 //			System.out.println("Username\tPassword");
 //			while(resultSet.next()) {
 //				System.out.println(resultSet.getString(1)+ "\t\t"+ resultSet.getString(2));
 //			}
-			Scanner scanner = new Scanner(System.in);
-			System.out.print("Enter username: ");
-			String userName = scanner.next();
-			System.out.print("Enter password: ");
-			String password = scanner.next();
-			int flag = 1;
-			while(resultSet.next()) {
-				if(userName.equals(resultSet.getString(1))  && password.equals(resultSet.getString(2))) {
-					System.out.println("Login successful");
-					flag = 0;
-				}
-			}
-			if(flag == 1) {
-				System.out.println("Invalid login creds");
-			}
+//			Scanner scanner = new Scanner(System.in);
+//			System.out.print("Enter username: ");
+//			String userName = scanner.next();
+//			System.out.print("Enter password: ");
+//			String password = scanner.next();
+//			int flag = 1;
+			
+//			while(resultSet.next()) {
+//				if(userName.equals(resultSet.getString(1))  && password.equals(resultSet.getString(2))) {
+//					System.out.println("Login successful");
+//					flag = 0;
+//					break;
+//				}
+//			}
+//			if(flag == 1) {
+//				System.out.println("Invalid login creds");
+//			}
+//			System.out.println();
+//			statement.executeUpdate("insert into login values('"+userName+"','"+password+"')");
+//			System.out.println("row added successfully");
+			
+			//ResultSet resultSet = statement.executeQuery("'insert into login values("+userName+","+password+")'")
+			// Above is invalid as you use execute query and result set when you get a response
+			
+			
+			// Update an account
+			
+//			Scanner scanner = new Scanner(System.in);
+//			System.out.print("Enter username which you want to update: ");
+//			String Uname = scanner.next();
+//			System.out.print("Enter new password: ");
+//			String newPass = scanner.next();
+//			String updateQuery = "update login set password='"+newPass+"' where username='"+Uname+"'";
+//			statement.executeUpdate(updateQuery);
+//			System.out.println("Password updated.");
+			
+			// Delete an account
+			
+//			Scanner scanner = new Scanner(System.in);
+//			System.out.print("Enter username which you want to delete: ");
+//			String Uname = scanner.next();
+//			String deleteQuery = "delete from login where username='"+Uname+"'";
+//			statement.executeUpdate(deleteQuery);
+//			System.out.println(Uname+" deleted.");
 			
 			
 		} catch (Exception e) {
